ffd15ea3dd0f509887daa7fde6c8f7aae442f02e
Slash20 <kurenkov200234@inbox.ru>
2025-04-02 15:17:00 +0200

    create table from db

--- src/main/java/ru/kors/students/entity/Student.java
+++ src/main/java/ru/kors/students/entity/Student.java
@@ -1,16 +1,27 @@
 package ru.kors.students.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Data
-@Builder
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
+    @Transient
     private int age;
     private LocalDate dateOfBirth;
+    @Column(unique = true)
     private String email;
+
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
 }
